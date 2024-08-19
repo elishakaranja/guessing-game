77ffad9f5e5b783ea374401787a954b170024892
elishakaranja <145563815+elishakaranja@users.noreply.github.com>
2024-08-20 00:10:34 +0300

    add exeption

--- guessing_game.py
+++ guessing_game.py
@@ -1,33 +1,26 @@
-import random 
+import random
 
-#guess number between 1 qnd 100
 def guess_number():
-    number_to_guess = random.randint(1,100)
-    attempts = 0 
-    
-    #create a loop to take in user input 
     while True:
-        try:
-            guess = int(input("guess number between 1 and 100"))
-            attempts += 1 
-        
-            if guess < number_to_guess:
-             print("Too low!")
-            elif guess > number_to_guess:
-             print("Too high!")
-            else:
-             print("Congratulations you guessed the correct number!")
-             
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            
+        # Generate a random number between 1 and 50
+        number_to_guess = random.randint(1, 50)
+        attempts = 0 
+
+        while True:
+            try:
+                # Get user input and track attempts
+                guess = int(input("Guess a number between 1 and 50: "))
+                attempts += 1 
+
+                if guess < number_to_guess:
+                    print("Too low!")
+                elif guess > number_to_guess:
+                    print("Too high!")
+                else:
+                    print(f"Congratulations! You guessed the correct number in {attempts} attempts!")
+                    break
+
+          
+
 if __name__ == "__main__":
-    
     guess_number()
-            
-        
-        
-        
-         
-    
-    
